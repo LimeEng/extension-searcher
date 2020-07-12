ff6e4db6515cf13c727abcd11acde099a67d4c69
Emil Englesson <englesson.emil@gmail.com>
2020-07-12 13:01:41 +0200

    Initial commit
    Signed-off-by: Emil Englesson <englesson.emil@gmail.com>

---
+++ README.md
@@ -0,0 +1,16 @@
+![](https://github.com/LimeEng/extension-searcher/workflows/CI/badge.svg)
+
+# extension-searcher
+
+This is a simple utility which will search a specified directory after files. The program will print the path to a file if the file has a extension matching a list of extensions provided by the user.
+
+- [Installation](#installation)
+
+## Installation
+
+Install the program by running
+```
+cargo install --git https://github.com/LimeEng/extension-searcher
+```
+
+It is also possible to download a pre-built binary for either Windows, Linux or macOS from the [releases-page](https://github.com/LimeEng/extension-searcher/releases).
---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use std::path::Path;
+use walkdir::WalkDir;
+
+#[macro_use]
+extern crate clap;
+use clap::{App, AppSettings, Arg};
+
+const CLI_SUB_CMD_SEARCH: &str = "search";
+const CLI_SUB_CMD_SEARCH_ARG_DIR: &str = "dir";
+const CLI_SUB_CMD_SEARCH_ARG_EXTENSIONS: &str = "extensions";
+
+#[derive(Debug)]
+pub enum CliError {
+    IoError(std::io::Error),
+    ValidationError(String),
+}
+
+impl From<std::io::Error> for CliError {
+    fn from(io_error: std::io::Error) -> Self {
+        CliError::IoError(io_error)
+    }
+}
+
+fn main() -> Result<(), CliError> {
+    let matches = App::new("extension-searcher")
+        // .version(crate_version!())
+        .long_version(crate_version!())
+        .version_short("v")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .subcommand(
+            App::new(CLI_SUB_CMD_SEARCH)
+                .about("Recursively searches the specified directory after files, with the specified extensions")
+                .arg(
+                    Arg::with_name(CLI_SUB_CMD_SEARCH_ARG_DIR)
+                        .help("The root directory")
+                        .index(1)
+                        .required(true),
+                )
+                .arg(
+                    Arg::with_name(CLI_SUB_CMD_SEARCH_ARG_EXTENSIONS)
+                        .help("The list of extensions that will be searched after")
+                        .index(2)
+                        .multiple(true)
+                        .required(true),
+                )
+        )
+        .get_matches();
+    match matches.subcommand() {
+        (CLI_SUB_CMD_SEARCH, Some(matches)) => {
+            let root_dir = matches.value_of(CLI_SUB_CMD_SEARCH_ARG_DIR).unwrap();
+            let extensions: Vec<_> = matches
+                .values_of(CLI_SUB_CMD_SEARCH_ARG_EXTENSIONS)
+                .unwrap()
+                .collect();
+            search_directory(root_dir.to_string(), |path| {
+                let extension = path.extension().and_then(|s| s.to_str());
+                if let Some(ext) = extension {
+                    if extensions.contains(&ext) {
+                        return true;
+                    }
+                }
+                return false;
+            });
+            Ok(())
+        }
+        _ => Err(CliError::ValidationError(String::from(
+            "Invalid subcommand",
+        ))),
+    }
+}
+
+fn search_directory<F: Fn(&Path) -> bool>(dir: String, should_print: F) {
+    for entry in WalkDir::new(dir).into_iter().filter_map(|e| e.ok()) {
+        if should_print(&entry.path()) {
+            println!("{}", entry.path().display())
+        }
+    }
+}
